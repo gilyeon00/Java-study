5f633c4a7f0f32a33cc59e7e0fd33836f65534f9
gilyeon <12venus8@naver.com>
2023-03-02 15:20:46 +0900

    add OpTest

---
+++ day01/src/day01/OpTest.java
@@ -0,0 +1,18 @@
+package day01;
+
+public class OpTest {
+
+	public static void main(String[] args) {
+		int a = 100;
+		int b = 3;
+		
+		System.out.println("a+b " + a+b);	// 1003
+		System.out.println("a+b " + (a+b));	// 103
+		System.out.println("a+b " + String.valueOf(a+b));	// 103
+		
+		System.out.println("a*b " + a*b);	// 300
+		
+
+	}
+
+}
