2b20fd9ad280bad7c28138e909d8d94264fc90db
gilyeon <12venus08@naver.com>
2023-03-03 14:29:09 +0900

    static example

--- day02/src/day02/Test4.java
+++ day02/src/day02/Test4.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class Test4 {
 
-	public static void main(String[] args) {
+	public static void main(String... args) {
 		Scanner sc = new Scanner(System.in);
 		
 		
--- day02/src/day02/Test5.java
+++ day02/src/day02/Test5.java
@@ -1,10 +1,32 @@
 package day02;
 
+import java.util.Random;
+
 public class Test5 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// 사용1
+		Random  r = new Random();
+		int num = r.nextInt();
+		System.out.println(num);
 
+		
+		// 사용2 - static메서드일 경우, class.메서드명()
+		int a = Math.abs(-34);
+		System.out.println("Math.abs(-34): " + a);
+//		static이 붙어있어서, new안써도됨 
+//		public static int abs(int a) {
+//	        return (a < 0) ? -a : a;
+//	    }
+		System.out.println("Math.random(): " + Math.random());
+		
+		
+		// String to Int
+		String b = "123";
+		int bb = Integer.parseInt(b);
+		System.out.println(bb);
+		
+		
 	}
 
 }
